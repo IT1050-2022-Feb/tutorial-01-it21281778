07a72ed45fd5a266231de2d797866f999f4a0283
P.K.D.L.W.Alwis <100864688+it21281778@users.noreply.github.com>
2022-03-04 17:01:02 +0000

    inputs marks for 2 Subjects . calculate average marks.

--- Tute01.c
+++ Tute01.c
@@ -6,6 +6,19 @@
 
 int main() {
   
+  float sub1, sub2, avg;
+
+  printf("Input marks for subject num 1:");
+  scanf("%f", &sub1);
+
+  printf("Input marks for subject num 2:");
+  scanf("%f", &sub2);
+
+  avg = (sub1+sub2)/2;
+
+  printf("average of 2 marks: %.2f", avg);
+
+
   return 0;
 }
 
